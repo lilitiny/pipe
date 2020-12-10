5c840a3e9c51ca13338153a1245757cbaf76f7b2
EC2 Default User <ec2-user@ip-172-31-28-125.ec2.internal>
2020-12-10 14:47:23 +0000

    dkfjdlkjf

--- app.py
+++ app.py
@@ -2,10 +2,14 @@
 
 from aws_cdk import core
 
-from piplines_webiner.piplines_webiner_stack import PiplinesWebinerStack
+from piplines_webiner.piplines_webiner_stack import PipelinesWebinarStack
+from piplines_webiner.pipeline_stack import PipelineStack
 
 
 app = core.App()
-PiplinesWebinerStack(app, "piplines-webiner")
-
+PipelinesWebinarStack(app, "pipelines-webinar")
+PipelineStack(app, 'PipelineStack', env={
+    'account': '900985145045',
+    'region': 'us-east-1'
+})
 app.synth()
--- piplines_webiner/piplines_webiner_stack.py
+++ piplines_webiner/piplines_webiner_stack.py
@@ -1,9 +1,33 @@
+from os import path
+
+
 from aws_cdk import core
+import aws_cdk.aws_lambda as lmb
+import aws_cdk.aws_apigateway as apigw
 
 
-class PiplinesWebinerStack(core.Stack):
+class PipelinesWebinarStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def init(self, scope: core.Construct, construct_id: str, kwargs) -> None:
+        super().init(scope, construct_id, kwargs)
+
+
 
         # The code that defines your stack goes here
+        this_dir = path.dirname(__file__)
+
+        handler = lmb.Function(self, 'Handler',
+            runtime=lmb.Runtime.PYTHON_3_7,
+            handler='handler.handler',
+            code=lmb.Code.from_asset(path.join(this_dir, 'lambda')))
+
+
+
+
+        gw = apigw.LambdaRestApi(self, 'Gateway',
+            description='Endpoint for a simple Lambda-powered web service',
+            handler=handler.current_version)
+
+
+        self.url_output = core.CfnOutput(self, 'Url',
+            value=gw.url)
